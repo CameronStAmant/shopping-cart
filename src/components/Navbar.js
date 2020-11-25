f4935d9af87a183687459c80cb77a1430346816a
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-25 11:50:54 -0700

    fix logo finally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = (props) => {
     <div className="nav">
       <img
         id="navLogo"
-        src="%PUBLIC_URL%/images/logo.png"
+        src="images/logo.png"
         alt="paws in the shape of a heart"
       />
       <ul className="navItems">
